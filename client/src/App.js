8ff9463cfecae201664a588623315e7d41cb326b
Paul Garton <paulgarton1984@hotmail.com>
2024-06-28 12:50:26 +0100

    add registration form functionality. Input value changes, handle form submit

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,20 @@ import Register from "./Pages/Register/Register";
 import Login from "./Pages/Login/Login";
 
 function App() {
+  async function addNewUserRegistration(name, email, password) {
+    const newUser = {
+      name: name,
+      email: email,
+      password: password,
+    };
+  }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route
+          path="/"
+          element={<Register registerUser={addNewUserRegistration} />}
+        />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
--- client/src/Pages/Register/Register.js
+++ client/src/Pages/Register/Register.js
@@ -1,29 +1,61 @@
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { React, useState } from "react";
 
-function Register() {
+function Register({ registerUser }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  function handleAddNewUser(e) {
+    e.preventDefault();
+
+    if (password !== passwordConfirm) {
+      console.error("Passwords do not match");
+      return;
+    }
+
+    registerUser(name, email, password);
+  }
   return (
     <div>
       <h2>Please enter registration details</h2>
-      <Form>
+      <Form onSubmit={handleAddNewUser}>
         <Form.Group controlId="nameRegistration">
           <Form.Label>Enter Name</Form.Label>
-          <Form.Control type="text"></Form.Control>
+          <Form.Control
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
         </Form.Group>
 
         <Form.Group controlId="emailRegistration">
           <Form.Label>Enter Email</Form.Label>
-          <Form.Control type="email"></Form.Control>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
         </Form.Group>
 
         <Form.Group controlId="passwordRegistration">
           <Form.Label>Enter Password</Form.Label>
-          <Form.Control type="text"></Form.Control>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
         </Form.Group>
 
         <Form.Group controlId="passwordConfirmationRegistration">
           <Form.Label>Confirm Password</Form.Label>
-          <Form.Control type="text"></Form.Control>
+          <Form.Control
+            type="password"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+          ></Form.Control>
         </Form.Group>
 
         <Button type="submit" variant="primary">
